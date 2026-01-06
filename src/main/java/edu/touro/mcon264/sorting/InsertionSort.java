938cdf90cf5df81a2e93749ca377a98bfe27332c
sonyaginzburg <sginzbur@student.touro.edu>
2026-01-05 21:16:43 -0500

    Implemented sorting algorithm

--- src/main/java/edu/touro/mcon264/sorting/InsertionSort.java
+++ src/main/java/edu/touro/mcon264/sorting/InsertionSort.java
@@ -6,6 +6,16 @@ public class InsertionSort implements Sorter {
 
     @Override
     public <T> void sort(T[] a, Comparator<? super T> comp) {
-        // TODO: implement insertion sort
+        for (int i = 1; i < a.length; i++) {
+            T key = a[i];
+            int j = i - 1;
+
+            // Shift elements
+            while (j >= 0 && comp.compare(a[j], key) > 0) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = key;
+        }
     }
 }
